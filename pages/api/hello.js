481d205bc1b5e9066d3b16a35fb457e146a2dbd6
peppermint100 <krpeppermint100@gmail.com>
2022-11-26 19:34:05 +0900

    added dotenv

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dotenv from "dotenv"
 
 export default function handler(req, res) {
   const { method } = req;
+  dotenv.config();
   
-  return res.status(200).json({ data: "hello world"});
+  return res.status(200).json({ data: process.env.SPOTIFY_AUTH_TOKEN});
 }
